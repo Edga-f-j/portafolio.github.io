34c5535aaa6cc07b96b10996db52dc0af84f4dc0
Edga-f-j <100364505+Edga-f-j@users.noreply.github.com>
2024-10-03 12:44:02 -0500

    Add files via upload

---
+++ TerceraSemana/js/app2.js
@@ -0,0 +1,42 @@
+
+let elemento = document.getElementById("miElemento");
+console.log(elemento);    
+elemento.textContent = "esto es javascript";
+elemento.style = "color:red";
+
+
+
+
+
+// function escala(x){
+//     for(let i=1;i<=20;i++){
+//                 x*=i;
+//                 console.log(i+". = "+x);
+
+//                 if(i==20){
+//                     for(let j=20;j>1;j--){
+//                         x/=j;
+//                         console.log((j-1)+". = "+x);
+//                         }      
+//                      }
+//     }
+// }
+
+// let y=10
+// escala(y);
+
+// let lista =[];
+
+
+// for(let carro of carros){
+//     if(carro.mpg>30){
+//         lista.push(carro.marca)
+//     }
+
+// }
+// console.log(lista)
+
+
+
+
+
